95b50a789b636969911d523ba525639407d1995b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 10:42:43 -0700

    import selectCounter in counter for use in hook selector

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,9 @@
 import { useSelector } from "react-redux";
+import { selectCounter } from "../features/CounterSlice";
+
 import Store from "../app/Store";
 const Counter = () => {
-  const counter = useSelector();
+  const counter = useSelector(selectCounter);
   return <div>Counter is {counter}</div>;
 };
 
