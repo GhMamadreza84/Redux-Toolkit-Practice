851fd151e2e6d91719fddf7b54682b12061894c7
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 11:45:59 -0700

    add new input and state for this

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 import { incrementByAmount, selectCounter } from "../features/CounterSlice";
 import { increment, decrement } from "../features/CounterSlice";
 import Store from "../app/Store";
 
 const Counter = () => {
+  const [value, setValue] = useState("");
   const counter = useSelector(selectCounter);
   const dispatch = useDispatch();
   return (
@@ -11,7 +13,14 @@ const Counter = () => {
       <h2>Counter is {counter}</h2>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(2))}>Increment By Amount</button>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementByAmount(2))}>
+        Increment By Amount
+      </button>
     </div>
   );
 };
