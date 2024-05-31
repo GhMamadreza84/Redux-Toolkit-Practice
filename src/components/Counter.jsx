ede4778f4819f021f673e8528e0159f25a389889
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 10:52:31 -0700

    add use dispatch in counter and use in onclick of button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectCounter } from "../features/CounterSlice";
-
+import { increment, decrement } from "../features/CounterSlice";
 import Store from "../app/Store";
+
 const Counter = () => {
   const counter = useSelector(selectCounter);
-  return <div>Counter is {counter}</div>;
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Counter is {counter}</h2>
+      <button onClick={() => dispatch(increment)}>Increment</button>
+      <button onClick={() => dispatch(decrement)}>Decrement</button>
+    </div>
+  );
 };
 
 export default Counter;
