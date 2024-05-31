615b532a2a86c74c3037f4fb147d9e902584f7e0
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 12:17:42 -0700

    edit store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import CounterReducer from "../features/counter/CounterSlice";
+import NumberReducer from "../features/number/NumberSlice";
 const Store = configureStore({
   reducer: {
     counter: CounterReducer,
+    number: NumberReducer,
   },
 });
 
