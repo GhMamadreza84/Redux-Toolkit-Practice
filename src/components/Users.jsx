d1bab90d762af8998bbd5d79c01bc47cdf9462d9
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 22:37:45 -0700

    edit users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../features/users/UserSlice";
 
 const Users = () => {
-  const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
   console.log(users)
   useEffect(() => {
     dispatch(fetchUsers());
