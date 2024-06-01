885d50d62b09f5fd098111eb64f8d47071fdbd71
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-05-31 22:28:06 -0700

    import and use userSlice as usersReducer in store and use it

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+
 import CounterReducer from "../features/counter/CounterSlice";
 import NumberReducer from "../features/number/NumberSlice";
+import usersReducer from "../features/users/UserSlice";
 const Store = configureStore({
   reducer: {
     counter: CounterReducer,
     number: NumberReducer,
+    users: usersReducer,
   },
-  middleware: (getDefualtMiddleWare) => getDefualtMiddleWare().concat(logger)
+  middleware: (getDefualtMiddleWare) => getDefualtMiddleWare().concat(logger),
 });
 
 export default Store;
